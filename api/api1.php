139a41ae6ca53ef09ad6acf6c8062f6618e84f43
EDGAR <edgarworkdev@gmail.com>
2025-01-18 21:24:49 +0300

    update1

--- api/api1.php
+++ api/api1.php
@@ -5,7 +5,7 @@
 	"launcher_name": "/launcher_space.apk",
 	"isTest": false,
 	"test_api": false,
-    "api": "https://cdn-space.spacerp.ru/api/main.json?edgar=1829top09",
+    "api": "https://raw.githubusercontent.com/edgardevwork/space-json-php/main/api/main.json?edgar=1829top09",
 	"archives": [
         {
             "type": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb",
