2399215a7c07eb4ad62a48082508fb5eb39aa648
EDGAR <edgarworkdev@gmail.com>
2025-01-18 21:17:29 +0300

    Initial commit

---
+++ api/Requests/vendor/connect.php
@@ -0,0 +1,7 @@
+<?php
+    require_once "rb.php";
+    // localhost менять на хост
+    // server1 менять на название бд(mysql)
+    // root менять на пользователя
+    // последнее менять на пароль
+    R::setup( 'mysql:host=149.202.88.119;dbname=gs253556','gs253556', 'cllcZZoJfkNk' );
